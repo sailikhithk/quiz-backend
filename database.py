36d7847bac7679c763ebf551529d033429c2886f
Harnath-FAI <harnath.a@foundationai.com>
2023-11-19 12:34:45 +0530

    POSTGRES SQL DB intigration

--- database.py
+++ database.py
@@ -1,22 +1,26 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 
 # Database URL configuration
-DATABASE_URL = "sqlite:///app.db"
+# DATABASE_URL = "sqlite:///app.db"
 
-# Create an engine instance using the configured DATABASE_URL
-# The engine is the starting point for any SQLAlchemy application.
+SQL_HOST = os.environ.get("SQL_HOST")
+SQL_PORT = os.environ.get("SQL_PORT")
+SQL_DB_NAME = os.environ.get("SQL_DB_NAME")
+SQL_DB_PASSWORD = os.environ.get("SQL_DB_PASSWORD")
+SQL_USER_NAME = os.environ.get("SQL_USER_NAME")
+
+DATABASE_URL = f"postgresql://{SQL_USER_NAME}:{SQL_DB_PASSWORD}@{SQL_HOST}:{SQL_PORT}/{SQL_DB_NAME}"
+
 engine = create_engine(DATABASE_URL)
 
-# Create a declarative base instance
-# The declarative base class serves as a catalog of classes that can be mapped to a relational database.
 Base = declarative_base()
 
-# Create a sessionmaker factory
-# The sessionmaker factory is used to create Session objects, which are the source of all ORM operations.
 Session = sessionmaker(bind=engine)
 
-# Create a Session instance
-# The session object allows you to add, update, delete, and query objects using SQLAlchemy ORM.
 session = Session()
